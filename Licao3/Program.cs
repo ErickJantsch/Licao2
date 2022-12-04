fc3ba349550dac13c297cda5e954eb841cebd431
Erick Jantsch <erickjantsch@gmail.com>
2022-12-04 02:04:44 +0000

    Licao 3 de C#

---
+++ Licao3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Licao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3) Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule \n" +
+                                  " mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula:\n" +
+                                  " DIFERENCA = (A * B - C * D).");
+            Console.WriteLine();
+            int A = 5;
+            int B = 6;
+            int C = 7;
+            int D = 8;
+            int DF = (A * B - C * D);
+            Console.WriteLine("A diferenca e " + DF);
+            Console.WriteLine();
+            int A1 = 5;
+            int B2 = 6;
+            int C3 = -7;
+            int D4 = 8;
+            int DF2 = A1 * B2 - C3 * D4;
+            Console.WriteLine("A diferenca e " + DF2);
+
+            
+        }
+    }
+}
